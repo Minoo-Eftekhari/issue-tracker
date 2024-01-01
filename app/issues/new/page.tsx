5f22bbdd1a9f8d018aac7bfbbe8cd2a26d6a5e24
Minoo Eftekhari <minoo.eftekhari5448@gmail.com>
2024-01-01 20:28:22 +0330

    Refactor : Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -25,6 +25,18 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      setSubmitting(false);
+      setError('An unexpected error occurred.');
+    }
+  });
 
   return (
     <div max-w-xl>
@@ -35,14 +47,7 @@ const NewIssuePage = () => {
       )}
       <form
         className=" space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setError("An unexpected error occured");
-          }
-        })}
+        onSubmit={onSubmit}
       >
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
