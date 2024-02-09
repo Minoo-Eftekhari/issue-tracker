46542fc525c43821ba03d8571b45c0f4d217882e
Minoo Eftekhari <minoo.eftekhari5448@gmail.com>
2024-02-09 11:45:03 +0330

    Add the login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,10 +5,14 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
+
 
 const NavBar = () => {
   const currentPath = usePathname();
-  console.log(currentPath);
+  const { status, data: session } = useSession();
+  
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -20,8 +24,9 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
+          <li key={link.href}>
           <Link
-            key={link.href}
+            
             className={classnames({
               "text-zinc-900": link.href === currentPath,
               "text-zinc-500": link.href !== currentPath,
@@ -31,8 +36,17 @@ const NavBar = () => {
           >
             {link.label}
           </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Log out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
+      </Box>
     </nav>
   );
 };
